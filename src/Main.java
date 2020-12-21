ff8bc6be0f30c33a895aa4d319ff45cf1cbc3b30
Matthew <matthew.d.baker20@gmail.com>
2020-12-21 00:13:30 -0600

    powerN problem

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,9 @@ public class Main {
         if(rows == 0) return 0;
         return rows + triangle(rows - 1);
     }
-
+    public static int powerN(int base, int n) {
+        if(n == 1) return base;
+        return base * powerN(base, n -1);
+    }
 
 }
