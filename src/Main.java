55988589639de666bf3b7de40ac4228d680ed2bb
Matthew <matthew.d.baker20@gmail.com>
2020-12-22 00:01:12 -0600

    bunnyears problem

--- src/Main.java
+++ src/Main.java
@@ -41,4 +41,10 @@ public class Main {
         return base * powerN(base, n -1);
     }
 
+    public static int bunnyEars(int bunnies) {
+        if(bunnies == 0) return 0;
+        if(bunnies == 1) return 2;
+        return 2 + bunnyEars(bunnies - 1);
+    }
+
 }
