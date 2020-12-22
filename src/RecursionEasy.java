0dc14a77d18c573e5cca1380d9e4a1c340283149
Matthew <matthew.d.baker20@gmail.com>
2020-12-22 00:13:39 -0600

    sleep in warm up problem

--- src/RecursionEasy.java
+++ src/RecursionEasy.java
@@ -1,4 +1,4 @@
-public class Main {
+public class RecursionEasy {
     public static void main(String[] args) {
 
         System.out.println(sumDigits(126)); // => 9
---
+++ src/Warmup1.java
@@ -0,0 +1,10 @@
+public class Warmup1 {
+    public static void main(String[] args) {
+        System.out.println("Warm up problems");
+    }
+
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return (!weekday || vacation);
+    }
+}
