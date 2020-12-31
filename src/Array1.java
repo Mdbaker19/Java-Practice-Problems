e0897c74382a56077818bc6d7f78ecf3ddd989b8
Matthew <matthew.d.baker20@gmail.com>
2020-12-31 13:46:55 -0600

    common end

--- src/Array1.java
+++ src/Array1.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Array1 {
     public static void main(String[] args) {
         System.out.println("Array 1 problems");
@@ -7,5 +11,10 @@ public class Array1 {
     }
     public boolean sameFirstLast(int[] nums) {
         return nums.length > 0 && nums[0] == nums[nums.length-1];
+    }
+    public static boolean commonEnd(int[] a, int[] b) {
+        int value1 = a[a.length-1];
+        int value2 = b[b.length-1];
+        return value1 == value2 || a[0] == b[0];
     }
 }
