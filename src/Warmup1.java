6759f92c3a01567ac9ff54df717565af7a5ab4cd
Matthew <matthew.d.baker20@gmail.com>
2021-01-01 22:54:22 -0600

    dif21 warmup

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -1,6 +1,8 @@
 public class Warmup1 {
     public static void main(String[] args) {
         System.out.println("Warm up problems");
+
+
     }
 
 
@@ -9,5 +11,9 @@ public class Warmup1 {
     }
     public int sumDouble(int a, int b) {
         return (a == b) ? (a + b) * 2 : a + b;
+    }
+
+    public int diff21(int n) {
+        return n > 21 ? Math.abs(n - 21) * 2 : Math.abs(n - 21);
     }
 }
