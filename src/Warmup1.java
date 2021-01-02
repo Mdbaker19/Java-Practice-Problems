a5f86e2199b813bb5a6473c4ac1dd8b0a5d56f91
Matthew <matthew.d.baker20@gmail.com>
2021-01-01 23:10:45 -0600

    every Nth char

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -60,4 +60,14 @@ public class Warmup1 {
         }
 
     }
+    public String everyNth(String str, int n) {
+        String output = "";
+
+        for(int i = 0; i < str.length(); i+=n){
+            output += str.charAt(i);
+        }
+
+        return output;
+
+    }
 }
