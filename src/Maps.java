d547cee58b294fafe1ae7c3c57a75ffa448f7747
Matthew <matthew.d.baker20@gmail.com>
2021-01-02 00:16:16 -0600

    ab2

--- src/Maps.java
+++ src/Maps.java
@@ -80,4 +80,15 @@ public class Maps {
         }
         return map;
     }
+    public Map<String, String> mapAB2(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")){
+            if(map.get("a").equals(map.get("b"))){
+                map.remove("a");
+                map.remove("b");
+            }
+        }
+
+        return map;
+    }
+
 }
