02b46830245cddde7448331af4581d45447f35db
Matthew <matthew.d.baker20@gmail.com>
2021-01-01 23:27:54 -0600

    coding bat hash maps mapbully

---
+++ src/Maps.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps {
+    public static void main(String[] args) {
+        HashMap<String, String> test = new HashMap<>();
+//        test.put("a", "candy");
+//        test.put("b", "dirt");
+//
+//        test.replace("b", test.get("a"));
+//        test.replace("a", "");
+//
+//        System.out.println(test);
+
+//        System.out.println(test.get("b") == null);
+
+
+    }
+
+    public Map<String, String> mapBully(Map<String, String> map) {
+
+        if(map.get("b") != null && map.get("a") != null){
+            map.replace("b", map.get("a"));
+            map.replace("a", "");
+        } else if(map.get("a") == null){
+            return map;
+        }else {
+            map.put("b", map.get("a"));
+            map.replace("a", "");
+        }
+        return map;
+    }
+}
