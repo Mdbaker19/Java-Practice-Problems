d1ee32ea069dc86dde6f003cd741cc976f9f0d1e
Matthew <matthew.d.baker20@gmail.com>
2021-01-02 00:12:51 -0600

    toppings 2

--- src/Maps.java
+++ src/Maps.java
@@ -60,4 +60,15 @@ public class Maps {
 
         return map;
     }
+    public Map<String, String> topping2(Map<String, String> map) {
+
+        if(map.containsKey("ice cream")){
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if(map.containsKey("spinach")){
+            map.put("spinach", "nuts");
+        }
+
+        return map;
+    }
 }
