4e29a28b18050e50cba122c1a35894442b291fac
Matthew <matthew.d.baker20@gmail.com>
2021-01-02 23:45:12 -0600

    string times

---
+++ src/Warmup2.java
@@ -0,0 +1,12 @@
+public class Warmup2 {
+    public static void main(String[] args) {
+
+    }
+    public String stringTimes(String str, int n) {
+        String out = "";
+        for(int i = 0; i < n; i++){
+            out += str;
+        }
+        return out;
+    }
+}
