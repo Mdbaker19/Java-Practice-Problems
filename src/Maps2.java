211a96016ede2b70d1493a576eb8e29a91d012cb
Matthew <matthew.d.baker20@gmail.com>
2021-01-03 13:11:54 -0600

    word0

---
+++ src/Maps2.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps2 {
+    public static void main(String[] args) {
+
+    }
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> out = new HashMap<>();
+        for (String string : strings) {
+            if (out.containsKey(string)) {
+                continue;
+            }
+            out.put(string, 0);
+        }
+        return out;
+    }
+
+
+}
