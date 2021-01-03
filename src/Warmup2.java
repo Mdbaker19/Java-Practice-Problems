87771b16f1394515331e0b1c470e2d85a343f648
Matthew <matthew.d.baker20@gmail.com>
2021-01-03 00:55:00 -0600

    arraycount9

--- src/Warmup2.java
+++ src/Warmup2.java
@@ -75,6 +75,14 @@ public class Warmup2 {
         return count;
 
     }
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        for(int num : nums){
+            if(num == 9) count++;
+        }
+        return count;
+    }
+
 
 
 }
