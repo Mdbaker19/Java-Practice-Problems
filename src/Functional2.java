036bbb9fc31b05d72faf80cf592fb3c085878ce0
Matthew <matthew.d.baker20@gmail.com>
2021-01-02 23:41:21 -0600

    two2

--- src/Functional2.java
+++ src/Functional2.java
@@ -3,6 +3,9 @@ import java.util.List;
 public class Functional2 {
     public static void main(String[] args) {
 
+
+
+
     }
     public List<Integer> noNeg(List<Integer> nums) {
         nums.removeIf(n -> n < 0);
@@ -41,6 +44,12 @@ public class Functional2 {
         strings.removeIf(s -> s.endsWith("y") || s.contains("yy"));
         strings.replaceAll(s -> s + "y");
         return strings;
+    }
+
+    public List<Integer> two2(List<Integer> nums) {
+        nums.replaceAll(n -> n * 2);
+        nums.removeIf(n -> n % 10 == 2);
+        return nums;
     }
 
 
