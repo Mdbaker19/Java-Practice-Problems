1853c7df11c327197781ff3981224957b1e77cd0
Matthew <matthew.d.baker20@gmail.com>
2021-01-03 00:05:16 -0600

    frontTimes

--- src/Warmup2.java
+++ src/Warmup2.java
@@ -9,4 +9,17 @@ public class Warmup2 {
         }
         return out;
     }
+    public String frontTimes(String str, int n) {
+        String sub = "";
+        if(str.length() > 2){
+            sub = str.substring(0, 3);
+        } else {
+            sub = str;
+        }
+        String out = "";
+        for(int i = 0; i < n; i++){
+            out += sub;
+        }
+        return out;
+    }
 }
