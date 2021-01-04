e9c3817d05516c47df8b69d8c8957a3d4318234c
Matthew <matthew.d.baker20@gmail.com>
2021-01-04 08:50:16 -0600

    scoresClump" "

--- src/AP1.java
+++ src/AP1.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class AP1 {
     public static void main(String[] args) {
 
@@ -19,5 +21,19 @@ public class AP1 {
             }
         }
         return false;
+    }
+    public boolean scoresClump(int[] scores) {
+        int count = 0;
+        for(int i = 0; i < scores.length - 2; i++){
+            if(check3(scores[i], scores[i + 1], scores[i + 2])) count++;
+        }
+        return count > 0;
+    }
+    public boolean check3(int a, int b, int c){
+        int minTemp = Math.min(a, b);
+        int min = Math.min(minTemp, c);
+        int maxTemp = Math.max(a, b);
+        int max = Math.max(maxTemp, c);
+        return (min + 2 >= max);
     }
 }
