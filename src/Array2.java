92f92d92b699afd782bad1848bfff93e9f7032da
Matthew <matthew.d.baker20@gmail.com>
2021-01-06 14:03:02 -0600

    big diff

--- src/Array2.java
+++ src/Array2.java
@@ -11,5 +11,14 @@ public class Array2 {
         }
         return count;
     }
+    public int bigDiff(int[] nums) {
+        int high = 0;
+        int low = 220;//not the best way
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > high) high = nums[i];
+            if(nums[i] < low) low = nums[i];
+        }
+        return high - low;
+    }
 
 }
