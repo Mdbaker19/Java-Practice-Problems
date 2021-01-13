e1a578e9a240ae4ea34aad04d9bcf6aaf4a1ae28
Matthew <matthew.d.baker20@gmail.com>
2021-01-12 20:49:17 -0600

    maximumWealth

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class LeetCode {
 
     public static void main(String[] args) {
@@ -14,6 +16,26 @@ public class LeetCode {
             out[i] = currSum;
         }
         return out;
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        ArrayList<Integer> all = new ArrayList<>();
+
+        for(int[] acct : accounts){
+            int total = 0;
+            for(int i = 0; i < acct.length; i++){
+                total+=acct[i];
+            }
+            all.add(total);
+        }
+        int highest = 0;
+        for(int x : all){
+            if(x > highest){
+                highest = x;
+            }
+        }
+
+        return highest;
     }
 
 
