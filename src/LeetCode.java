1d79c693c4ee1c5e8a7d5fc2e1e15b0f6409f58b
Matthew <matthew.d.baker20@gmail.com>
2021-01-14 11:51:22 -0600

    range sum bst

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,3 +1,4 @@
+import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -338,5 +339,36 @@ public class LeetCode {
             matches+=(n/2);
             return matches + numberOfMatches(n/2);
         }
+    }
+
+
+    public static class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+
+        if(root.val >= low && root.val <= high){
+            sum+=root.val;
+        }
+
+        if(root.left != null){
+            sum+=rangeSumBST(root.left, low, high);
+        }
+        if(root.right != null){
+            sum+=rangeSumBST(root.right, low, high);
+        }
+
+        return sum;
+
     }
 }
